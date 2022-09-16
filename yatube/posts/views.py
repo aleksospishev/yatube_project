de508899350aa7bad7b67f314e7bfc5949293041
Александр Оспищев <Aleksospishev@yandex.ru>
2022-09-16 23:18:28 +0300

    финиш

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,23 @@
-from django.http import HttpResponse
-
+# posts/views.py
+from django.shortcuts import render
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post
 
 def index(request):
-    return HttpResponse('Hi, luck!')
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
-
-def group(request):
-    return HttpResponse('Star Wars')
-
-
-# В урл мы ждем парметр, и нужно его прередать в функцию для использования
-def group_pack(request, slug: 'dv'):
-    return HttpResponse('dart veider')
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title' : title,
+    }
+    return render(request, template, context)
