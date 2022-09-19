ec08f596055383980ce4e455d35d9839049feb3f
Александр Оспищев <Aleksospishev@yandex.ru>
2022-09-19 20:00:57 +0300

    тесты

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,6 +4,15 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=40, unique=True)
+    descriptions = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -12,3 +21,12 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
+
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+    )
+
+
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -73,8 +73,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'yatube.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+    #Database
+#https://docs.djangoproject.com/en/2.2/ref/settings/
 
 DATABASES = {
     'default': {
